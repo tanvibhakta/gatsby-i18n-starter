3be145a6511abe36ff10d696d98abafca7434815
Tanvi Bhakta <tanvibhakta@gmail.com>
2018-11-13 14:18:55 +0530

    fix data to show only one page at a time

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,13 +2,19 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
+import { Link } from 'gatsby';
 
 export default ({ data }) => (
-  <Layout>
-    <h1>{data.allMarkdownRemark.edges.node.frontmatter.title}</h1>
-    <p>Hello</p>
-</Layout>
-)
+    <Layout>
+      <h2>{data.allMarkdownRemark.edges[1].node.frontmatter.title}</h2>
+      <p dir='rtl'>{data.allMarkdownRemark.edges[2].node.rawMarkdownBody}</p>
+      <Link to='/'> 
+        <p>{window.location.href}</p>
+      </Link>
+  </Layout>
+  )
+
+
  
 
 export const query = graphql`
