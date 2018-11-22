5913e377c6e5f0c202779f2c22f867adcd7c8ffa
Tanvi Bhakta <tanvibhakta@gmail.com>
2018-11-22 16:48:15 +0530

    add functions which are supposed to create language-ful page paths

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,44 @@
  */
 
 // You can delete this file if you're not using it
+
+// Take the pages from src/pages and generate pages for all locales, e.g. /blog and /en/blog
+const path = require(`path`)
+
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+    return new Promise((resolve, reject) => {
+        resolve(
+            graphql(`
+            {
+                site {
+                    siteMetadata {
+                        languages
+                    }
+                }
+
+                allSitePage {
+                    edges {
+                        node {
+                            path
+                        }
+                    }
+                }   
+            }
+        `).then(result => {
+            result.data.site.siteMetadata.languages.map((language) => {
+                result.data.allSitePage.edges.map(({ node }) => {
+                    createPage({
+                        path: '/' + language + node.path,
+                        component: path.resolve(`./src/components/layout.js`),
+                        context: {
+                            language: language,
+                        }, 
+                    })
+                })
+            });
+        })
+        )
+        
+    })
+} 
