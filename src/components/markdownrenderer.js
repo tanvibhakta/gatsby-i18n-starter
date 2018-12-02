e5f877b23006fc4673f602336ecb89aaf4ede3b0
Tanvi Bhakta <tanvibhakta@gmail.com>
2018-12-03 00:49:47 +0530

    Refactor markdownRenderer to use LanguageContext

--- src/components/markdownrenderer.js
+++ src/components/markdownrenderer.js
@@ -1,32 +1,41 @@
 import React from 'react'
 
 import { StaticQuery, graphql } from 'gatsby'
+import { Consumer } from '../context/createContext';
 
-const MarkdownRenderer = props => {
-    let lang = props.lang
+            //TODO: reformat query to find file on basis of language/
+            //Maybe adding a filter to the query based on file structure?
+
+        //  Everything would have been hunky dory if the nodes were created 
+        //  in the order of declaration of the languages. Unfortunately 
+        //  that is not how any of this works. :(   
+const MarkdownRenderer = () => {
     return (
-        //TODO: reformat query to find file on basis of language/
-        //Maybe adding a filter to the query based on file structure?
-        <StaticQuery
-            query={graphql`
-            {
-            allMarkdownRemark {
-                edges {
-                node {
-                    frontmatter {
-                    title
+            <StaticQuery
+                query={graphql`
+                {
+                allMarkdownRemark {
+                    edges {
+                    node {
+                        frontmatter {
+                        title
+                        }
+                        rawMarkdownBody
                     }
-                    rawMarkdownBody
+                    }
                 }
                 }
-            }
-            }
-            `}
-            render={data => (
-                    <p>{data.allMarkdownRemark.edges[lang].node.rawMarkdownBody}</p>
-            )}
-        />
-
+                `}
+                render={data => (
+                    <Consumer>
+                        {({langIndex}) => {
+                            console.log(langIndex)
+                            return(
+                            <p>{data.allMarkdownRemark.edges[3].node.rawMarkdownBody}</p>
+                            )}}
+                    </Consumer>
+                )}
+            />
     )
 }
     
