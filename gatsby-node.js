90fe6c85f88cc487bfd6dd0f482ec529441a1e0d
Tanvi Bhakta <tanvibhakta@gmail.com>
2018-12-04 11:33:23 +0530

    create urls with all languages and point to the respective page :star:

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,50 +7,44 @@
 // You can delete this file if you're not using it
 
 // Take the pages from src/pages and generate pages for all locales, e.g. /blog and /en/blog
-const langList = [];
-const nodeList = [];
+const langList = []
+const nodeList = []
 
 exports.createPages = ({ graphql }) => {
-    return new Promise(( resolve ) => {
-        resolve(
-            graphql(`
-            {
-                site {
-                    siteMetadata {
-                        languages
-                    }
-                }
+  return new Promise(resolve => {
+    resolve(
+      graphql(`
+        {
+          site {
+            siteMetadata {
+              languages
             }
-        `).then(result => {
-            result.data.site.siteMetadata.languages.map((language) => {
-                langList.push(language)
-                console.log(langList);
-            });
+          }
+        }
+      `).then(result => {
+        result.data.site.siteMetadata.languages.map(language => {
+          langList.push(language)
+          console.log(langList)
         })
-        )
-    })
-} 
+      })
+    )
+  })
+}
 
 exports.onCreatePage = ({ page, actions }) => {
-    const { createPage, deletePage } = actions
-    return new Promise(resolve => {
-      const oldPage = Object.assign({}, page)
-      
-      // Add all languages from the list
-      langList.forEach( (language) => {
-              console.log("BEFORE ----"+page.path);
-              page.path = '/' + language + page.path;
-              console.log("AFTER ----- "+ page.path);
-                
-      })
+  const { createPage, deletePage } = actions
+  return new Promise(resolve => {
+    const oldPage = Object.assign({}, page)
 
-      if (page.path !== oldPage.path) {
-        // Replace new page with old page
-        deletePage(oldPage)
-        createPage(page)
-        console.log("FINAL-------------"+ page.path);
-        
-      }
-      resolve()
+    // Add all languages from the list
+    var ogpath = page.path
+    langList.forEach(language => {
+      console.log('BEFORE ----' + page.path)
+      page.path = '/' + language + ogpath
+      createPage(page)
+      console.log('AFTER ----- ' + page.path)
     })
-  }
+    deletePage(oldPage)
+    resolve()
+  })
+}
